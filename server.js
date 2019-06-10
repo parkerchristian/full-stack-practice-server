c004164bc11a7545a26dcccf2f4650d26b7ccd8b
Parker Helland <hellandp@gmail.com>
2019-06-10 13:37:10 -0700

    server, connect and app connected properly

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 
-const app = require('app');
+const app = require('./lib/app');
 
 const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Listening on ${PORT}`);
 });
