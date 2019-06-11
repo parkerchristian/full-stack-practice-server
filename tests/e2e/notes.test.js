790becfc477f334005741f8a67f8840ec1c81ba3
Parker Helland <hellandp@gmail.com>
2019-06-11 10:27:51 -0700

    GET route written

--- tests/e2e/notes.test.js
+++ tests/e2e/notes.test.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 const connect = require('../../lib/utils/connect');
 const app = require('../../lib/app');
 
+const createNote = note => {
+  return request(app)
+    .post('/api/v1/notes')
+    .send(note)
+    .then(res => res.body);
+};
+
 describe('NOTE ROUTES TESTS', () => {
   beforeAll(() => {
     return connect();
@@ -26,6 +33,18 @@ describe('NOTE ROUTES TESTS', () => {
           body: 'body of note',
           __v: 0
         });
+      });
+  });
+
+  it('GET a list of notes', async() => {
+    const notes = await Promise.all([
+      createNote({ title: 'title', body: 'body' }),
+      createNote({ title: 'title', body: 'body' })
+    ]);
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toEqual(notes);
       });
   });
 
